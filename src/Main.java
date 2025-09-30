846258e4e04970fb373e62198cc5653e23b0597d
EstherNawaday <esthern36@nycstudents.net>
2025-09-30 13:18:55 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        boolean[][] schedule = new boolean[8][60];
+        Appointmentbook a = new Appointmentbook(schedule);
+        for(int i = 30; i < 45; i++) schedule[1][i] = true;
+        a.printPeriod(2);
+        a.findFreeBlock(2,15);
+        Appointmentbook b = new Appointmentbook(schedule);
+        int period = 2;
+        while(period < 5){
+            System.out.println("Period: " + period);
+            b.printPeriod(period);
+            period++;
+        }
+    }
+}
