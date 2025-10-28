648258d29e3e30e83cc873b80803664b4db96d46
EstherNawaday <esthern36@nycstudents.net>
2025-10-28 13:19:17 -0400

    appbook

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -17,5 +21,27 @@ public class Main {
         b.printPeriod(4);
         System.out.println(b.makeAppointment(2, 4, 22));
         b.printPeriod(4);
+    }
+    public static int read() throws FileNotFoundException{
+        int meetings = 0;
+        File f = new File("Schedules.txt");
+        Scanner s = new Scanner(f);
+        String schedule = "";
+        while (s.hasNext()){
+            while (s.hasNextBoolean()){
+                schedule += s.nextBoolean() + "";
+            }
+        }
+        Appointmentbook a = new Appointmentbook(readSchedule(schedule));
+    }
+    public static boolean[][] readSchedule(String lines){
+        Scanner s = new Scanner(lines);
+        boolean[][] schedule = new boolean[8][60];
+        for (int i = 0; i < schedule.length; i++){
+            for (int j = 0; j < schedule[i].length; j++){
+                schedule[i][j] = s.nextBoolean();
+            }
+        }
+        return schedule;
     }
     }
